79310696a1ce1488ff5140f02e10ba71d7c3837b
hemantku2015@gmail.com <95603296+hemantj55@users.noreply.github.com>
2022-10-13 19:19:15 +0530

    operators

---
+++ operator.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//Arithmetic operators
+
+$a=10;
+$b=30;
+$c=$a+$b;
+$d=$b-$a;
+$e=$b*$a;
+$f=$b/$a;
+$g=5;
+$h=7;
+echo "\nadd: ".$c;
+echo "<br>";
+echo "\nsub: ".$d;
+echo "<br>";
+echo "\nmult: ".$e;
+echo "<br>";
+echo "\ndiv: ".$f;
+echo "<br>";
+$g++;
+echo  "increment: ".$g;
+echo "<br>";
+$h--;
+ echo "decrement: ".$h;
+?>
+</body>
+</html>
